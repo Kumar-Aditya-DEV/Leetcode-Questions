282e1dad02b87856ec3ff01e926ce57fec0c95a7
Aditya Kumar <aditya.kumar.j.cg@gmail.com>
2026-08-07 13:20:27 +0530

    Time: 0 ms (100%), Space: 9.7 MB (81.58%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+         string s = "1";
+        for(int i = 1; i < n; i++) {
+            string temp = "";
+            int j = 0;
+            while(j < s.size()) {
+                int count = 1;
+                while(j + 1 < s.size() && s[j] == s[j + 1]) {
+                    count++;
+                    j++;
+                }
+                temp += to_string(count);
+                temp += s[j];
+                j++;
+            }
+            s = temp;
+        }
+        return s;
+    }
+};
