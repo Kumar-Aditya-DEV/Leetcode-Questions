f0d770c61649606f26e852d0670b71774317a5c6
Aditya Kumar <aditya.kumar.j.cg@gmail.com>
2026-09-21 17:14:27 +0530

    Time: 4 ms (33.72%), Space: 9.7 MB (74.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,7 +4,6 @@ public:
         if(s.length() != t.length()) return false;
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        if(s == t) return true;
-        else return false;
+        return (s == t);
     }
 };
